acc327925fd97a91a6146c88bff527beb755d87e
matsta25 <matim2112@gmail.com>
2020-12-01 16:58:04 +0100

    feat(app): add primitive sh v8

--- src/main/java/com/matsta25/trimagebackend/utlis/RenderUtil.java
+++ src/main/java/com/matsta25/trimagebackend/utlis/RenderUtil.java
@@ -31,7 +31,7 @@ public class RenderUtil {
 //        ProcessBuilder builder = new ProcessBuilder("/app/go/bin/primitive", "-i", "static/photos/" + fileName, "-o", "static/photos/output_" + fileName, "-n", String.valueOf(numberOfShapes), "-v");
 
         ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command("bash", "-c", "primitive");
+        processBuilder.command("/bin/bash", "-c", "ls -all && ls -all /app/ && ls -all /app/.go/ ");
         processBuilder.redirectErrorStream(true);
         final Process process = processBuilder.start();
         watchProgress(process, fileName);
