3c5e66b9c131ee391e4d447216c68be0006f8453
matsta25 <matim2112@gmail.com>
2020-12-01 15:44:04 +0100

    feat(app): add primitive sh v4

--- src/main/java/com/matsta25/trimagebackend/TrimageBackendApplication.java
+++ src/main/java/com/matsta25/trimagebackend/TrimageBackendApplication.java
@@ -8,23 +8,36 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 @SpringBootApplication
 public class TrimageBackendApplication implements CommandLineRunner {
 
 
-	Logger logger = LoggerFactory.getLogger(RenderUtil.class);
+    Logger logger = LoggerFactory.getLogger(RenderUtil.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(TrimageBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TrimageBackendApplication.class, args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		logger.info("TrimageBackendApplication: run()");
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info("TrimageBackendApplication: run()");
 
-		ProcessBuilder processBuilder = new ProcessBuilder();
-		processBuilder.command("bash", "-c", "go get -u github.com/fogleman/primitive");
-		processBuilder.command("bash", "-c", "primitive");
-		processBuilder.start();
-	}
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command("bash", "-c", "go get -u github.com/fogleman/primitive");
+//		processBuilder.command("bash", "-c", "primitive");
+        var process = processBuilder.start();
+
+        try (var reader = new BufferedReader(
+                new InputStreamReader(process.getInputStream()))) {
+
+            String line;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        }
+    }
 }
